1bddba35850f5620ccb118313b2c97d619c4c9d7
irlgabriel <radu.gabriel131@gmail.com>
2020-09-21 19:51:02 +0300

    added complete/uncomplete feature

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,10 +5,19 @@ const Todo = (props) => {
     console.log(props.todo)
     props.setTodos(props.todos.filter(el => el.id !== props.todo.id) )
   }
+  const completeHandler = () => {
+    props.setTodos(props.todos.map(el => {
+      if(el.id === props.todo.id) {
+        return {
+          ...el, completed: !el.completed
+        }
+      }
+    }))
+  }
   return (
     <div className="todo">
       <li className="todo-item">{props.text}</li>
-      <button className="complete-btn">
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
       <button onClick={deleteHandler} className="trash-btn">
